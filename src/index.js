c343e2b35f9a0fa973d4689930e903981d808883
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-08 00:21:23 +0100

    route params (find user by his unique id dynamicly), first we need to parseint the  id comming from the request as param then we compare it with fake data array we have with .find methode then we check if the user with the given id deos not exist we return with res.status(400).send  if exist we return res.send()

--- src/index.js
+++ src/index.js
@@ -2,15 +2,37 @@ import express from "express";
 
 const app = express();
 
+const fakeUsers = [
+  { id: 1, name: "soth" },
+  { id: 2, name: "jack" },
+];
+
 app.get("/", (req, res) => {
   res.status(201).send("Hello, World!"); //sending simpple text
 });
 
+// all users
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 1, name: "soth" },
-    { id: 2, name: "jack" },
-  ]); // sending json object
+  res.send(fakeUsers); // sending json object
+});
+
+app.get("/api/users/:id", (req, res) => {
+  // 2 methods to transfer the parameter from string to number
+  const parsedUserIdMethode1 = req.params.id * 1;
+  const parsedUserIdMethode2 = parseInt(req.params.id);
+
+  // if the parameter given not a number
+  if (isNaN(parsedUserIdMethode1)) {
+    return res.status(400).send({ msg: "Invalid user id" });
+  }
+
+  // find the user by id with array method (find)
+  const findUser = fakeUsers.find((user) => user.id === parsedUserIdMethode1);
+
+  // if the parameter given (id) is indeed a number but does not exist in the array
+  if (!findUser) 
+  return res.status(404).send({ msg: "User not found" });
+  res.send(findUser);
 });
 
 app.listen(3000, () => {
