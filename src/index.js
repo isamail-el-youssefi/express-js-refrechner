f9bb8cae9ad242be0552458c8fd1f63a18d32660
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-08 02:24:04 +0100

    qury params very important

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
-import express from "express";
+import express, { query } from "express";
 
 const app = express();
 
 const fakeUsers = [
-  { id: 1, name: "soth" },
-  { id: 2, name: "jack" },
+  { id: 1, name: "soth", car: "fiord" },
+  { id: 2, name: "smail", car: "toyota" },
+  { id: 3, name: "hamza", car: "honda" },
+  { id: 4, name: "sof1", car: "rav4" },
+  { id: 5, name: "mourad", car: "dacia" },
+  { id: 6, name: "marra", car: "renault" },
+  { id: 7, name: "imad", car: "rand rover" },
 ];
 
 app.get("/", (req, res) => {
   res.status(201).send("Hello, World!"); //sending simpple text
 });
 
-// all users
-app.get("/api/users", (req, res) => {
-  res.send(fakeUsers); // sending json object
-});
-
+//?? Route Params  localhost:3000/users/:id
 app.get("/api/users/:id", (req, res) => {
+  console.log(req.params); // {id: "1"}
+  console.log(req.params.id); // 1
   // 2 methods to transfer the parameter from string to number
   const parsedUserIdMethode1 = req.params.id * 1;
   const parsedUserIdMethode2 = parseInt(req.params.id);
@@ -30,9 +33,25 @@ app.get("/api/users/:id", (req, res) => {
   const findUser = fakeUsers.find((user) => user.id === parsedUserIdMethode1);
 
   // if the parameter given (id) is indeed a number but does not exist in the array
-  if (!findUser) 
-  return res.status(404).send({ msg: "User not found" });
+  if (!findUser) return res.status(404).send({ msg: "User not found" });
   res.send(findUser);
+});
+
+//?? Query params localhost:3000/users?filter=value&value=value
+
+app.get("/api/users", (req, res) => {
+  console.log(req.query);
+  const { filter, value } = req.query;
+  const filterByValue = fakeUsers.find((user) => user[filter].includes(value));
+
+  if (!filterByValue) return res.status(400).send({ msg: "Invalid value" });
+
+  // when filter and value are provided
+  if (filter && value) return res.send(filterByValue);
+  //localhost:3000/api/users?filter=name&value=so ===>>>  [{"id":1,"name":"soth","car":"fiord"},{"id":4,"name":"sof1","car":"rav4"}]
+
+  // if filter and value are not provided
+  http: res.send(fakeUsers);
 });
 
 app.listen(3000, () => {
