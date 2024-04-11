1233da26fd9b6848bc5d59ebca947532d15eca39
Ismail El YOUSSEFI <elyoussefiismail18@gmail.com>
2024-04-11 22:39:33 +0100

    setting up session with package express-session added new route for fake authentification by comparing the name and password the client sending as body in the post request with names and passwords in the array if it matches one client get authenticated succesfully

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import userRouter from "./routes/users.mjs";
 import productRouter from "./routes/product.mjs";
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import { fakeUsers } from "./utils/data.mjs";
 
 // Instantiating express to variable app
 const app = express();
@@ -9,6 +11,16 @@ const app = express();
 // Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
 app.use(express.json());
 app.use(cookieParser("randomsecret"));
+app.use(
+  session({
+    secret: "ismailelyoussefiblabla",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 20000,
+    },
+  })
+);
 
 const globalMiddleware = (req, res, next) => {
   console.log(req.method, req.path, "HI IAM MIDDLEWARE");
@@ -29,12 +41,29 @@ const localMiddleware = (req, res, next) => {
 app.get("/", localMiddleware, (req, res) => {
   console.log(req.cookies);
   console.log(req.header.cookie);
+  // we did take the cookie from the server responce bz the server who is creatin the cookie
   res.cookie("cookieExperimental", "hello im a cookie", {
     maxAge: 15000,
     signed: true,
   }); // this is the the route ("/") that you must visit first in order for the user to kind like authenticate have the cookies
   res.status(201).send("Hello, World!"); //sending simpple text
 });
+
+//!! Fake authentication to understand well the session concept
+app.post("/api/auth", (req, res) => {
+  const { name, password } = req.body;
+  const findUser = fakeUsers.find(
+    (user) => user.name === name && user.password === password
+  );
+  if (!findUser) return res.status(401).send({ msg: "BAD CREDENTIALS" });
+
+  // dynamic property (user) after the req.session {chof modkira for better understanding}
+  // req.session.user is being dynamically assigned a value during the execution of the code.
+  req.session.user = findUser; // setting up the cookie
+  return res.status(200).send({ msg: "AUTHENTICATED" });
+});
+
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
--- src/utils/data.mjs
+++ src/utils/data.mjs
@@ -1,9 +1,9 @@
 export const fakeUsers = [
-  { id: 1, name: "soth", car: "fiord" },
-  { id: 2, name: "smail", car: "toyota" },
-  { id: 3, name: "hamza", car: "honda" },
-  { id: 4, name: "sof1", car: "rav4" },
-  { id: 5, name: "mourad", car: "dacia" },
-  { id: 6, name: "marra", car: "renault" },
-  { id: 7, name: "imad", car: "rand rover" },
+  { id: 1, name: "ismail", car: "fiord", password: "hello123" },
+  { id: 2, name: "smail", car: "toyota", password: "hello1234" },
+  { id: 3, name: "hamza", car: "honda", password: "hello123" },
+  { id: 4, name: "sof1", car: "rav4", password: "hello123" },
+  { id: 5, name: "mourad", car: "dacia", password: "hello123" },
+  { id: 6, name: "marra", car: "renault", password: "hello123" },
+  { id: 7, name: "imad", car: "rand rover", password: "hello123" },
 ];
